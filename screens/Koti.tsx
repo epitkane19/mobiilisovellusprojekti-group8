7edbc2616cde59c39e3e5549caa8e20bcaca6f08
TomPohjola <tom1996666@gmail.com>
2026-03-16 19:17:07 +0200

    matikka fixit + test

--- screens/Koti.tsx
+++ screens/Koti.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import { laskeAvgNopeus, laskeLenkinKalorit, laskeJuoksujenAvgMatka } from '../mathFunctions/functions';
 
 export function Koti() {
+
+  const[TempResult, setTempResult] = useState(0)
+
   return (
     <View style={style.container}>
-      <Text style={style.text}>Eka ruutu</Text>  
+      <Button
+      onPress={() => setTempResult(laskeAvgNopeus(0, 5, 0, 14))}
+      title="avg nopeus testi (5 sekuntia 14 metriä"
+      color="#841584"
+      ></Button>
+      <Button
+      onPress={() => setTempResult(laskeLenkinKalorit(70, 60, 2.5))}
+      title="kalorit testi (70kg 60min 2.5mps)"
+      color="#841584"
+      ></Button>
+      <Button
+      onPress={() => setTempResult(laskeJuoksujenAvgMatka([7.54, 5, 12.54, 8.6]))}
+      title="avg matka testi [7.54km, 5km, 12.54km, 8.6km]"
+      color="#841584"
+      ></Button>
+      <Text>{TempResult}</Text>
     </View>
   );
 }
